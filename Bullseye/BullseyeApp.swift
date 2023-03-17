e23099ea18411374d987e732d60e7562bb17a390
muranhu <extern.muran.hu@mobilityasia.com.cn>
2023-03-17 09:27:38 +0800

    Initial Commit

---
+++ Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by Muran Hu on 2023/3/17.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
